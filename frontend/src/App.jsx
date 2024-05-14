edce5897d807a5695e93af0a0412404c215f3284
maldikurniawan <akurniawan2102@gmail.com>
2024-05-14 13:57:30 +0700

    fix bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import IsLogin from "./features/auth/isLogin";
+import Layout from "./components/shared/Layout";
 
 export default function App() {
   return (
     <Routes>
       <Route element={<IsLogin />}>
-        <Route path="/" element={<Dashboard />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Layout />}>
+          <Route index element={<Dashboard />} />
         </Route>
       </Route>
       <Route path="/login" element={<Login />} />
