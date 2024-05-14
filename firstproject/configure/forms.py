cca76e23df3e72ad9b598e076070a32c18819416
maldikurniawan <akurniawan2102@gmail.com>
2024-05-14 09:51:08 +0700

    update nama file

--- firstproject/configure/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CustomUser
-
-class LoginForm(forms.ModelForm):
-    class Meta:
-        model = CustomUser
-        fields = ['email', 'nama_perusahaan', 'nomor_telepon', 'status_login', 'last_login', 'session_timeout']
